e077bff96c1bd6c2a7a30874bcea7f0e412c0ec0
Teresa DAnna <teresadanna@Teresas-MacBook-Pro.local>
2017-07-20 13:13:44 -0700

    updates

--- chptr_11.rb
+++ chptr_11.rb
@@ -1,3 +1,41 @@
+puts "Question - Safer picture downloading"
+puts ""
+Dir.chdir '/Users/teresadanna/Library/Mobile Documents/com~apple~CloudDocs/Documents/to/'
+pic_names = Dir['/Users/teresadanna/Library/Mobile Documents/com~apple~CloudDocs/Documents/from/']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+puts
+print "Downloading #{pic_names.length} files"
+pic_number = 1
+pic_names.each do |name|
+  print '.'
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+  File.rename name, new_name
+  pic_number = pic_number + 1
+end
+
+puts "done importing!"
+puts "Not done with this one - need to understand what he did"
+
+puts "Question - Build your own playlist"
+
+filename = 'playlist.txt'
+music_files = Dir['**/*.mp3' ]
+
+File.open filename, 'w' do |f|
+  music_files.each do |music|
+    f.write music+"\n"
+  end
+end
+puts File.read filename
+
+
+puts ""
+puts "Question - Build a better playlist"
 music = ['hello kitty', 'Sarcastic world', 'leboff eric', 'cant think of names', 'anogther one', 'one more', 'last song name']
 
 
